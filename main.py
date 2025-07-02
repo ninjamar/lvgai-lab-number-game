ced27176b8bdd1b1e517c9f40b66a5fde877f8ca
ninjamar <70116773+ninjamar@users.noreply.github.com>
2025-07-02 17:34:35 -0500

    Add stretch goal implementation

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ while True:
     if guess == n:
         print("You guessed it!")
         print("You guessed it in", guesses, "guesses")
+        
+        if guesses > min_guesses:
+            min_guesses = guesses
         if min_guesses > 0:
             print("Best score: ", min_guesses)
 
         play_again = input("Would you like to play again? (yes/no): ")
         if play_again.lower() != "yes":
             break
-        if guesses > min_guesses:
-            min_guesses = guesses
+
 
         n = random.randint(1, 100)
         guesses = 0
